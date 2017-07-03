66611fd3dcd0c0c6970ca175e4291347e0936331
lyuhao <lyhumji@gmail.com>
2017-07-03 16:38:21 -0400

    haha

---
+++ run.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+SPARK_HOME=$HOME/spark_scripts
+ONLINE_HOME=$HOME/tailbench-v0.9/$2
+# setup 
+PER_COUNTER_HOME=$HOME/scripts/bash_scripts/IntelPerformanceCounterMonitorV2.8/
+
+$SPARK_HOME/run_$1.sh $3
+echo $! > spark.pid
+
+$ONLINE_HOME/run_networked.sh $4 $5
+
+PER_COUNTER_HOME/pcm.x -csv=$1_$2_$3_$4.csv -i=300000 &
+
+echo $! > pcm.pid
+
+wait ${cat spark.pid}
+
+$ONLINE_HOME/kill_networked.sh
+
+sudo kill -9 ${cat pcm.pid}
+
+
+
+
