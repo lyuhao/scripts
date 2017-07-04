ba6d787af6e403697390f472344cac572b27d4a8
yl408 <yl408@duke.edu>
2017-07-04 12:47:45 -0400

    testing

--- run.sh
+++ run.sh
@@ -1,25 +1,26 @@
 #! /bin/bash
 
 
-SPARK_HOME=$HOME/spark_scripts
+SPARK_HOME=$HOME/summer_scripts/spark_scripts
 ONLINE_HOME=$HOME/tailbench-v0.9/$2
 # setup 
 PER_COUNTER_HOME=$HOME/scripts/bash_scripts/IntelPerformanceCounterMonitorV2.8/
 
-$SPARK_HOME/run_$1.sh $3
-echo $! > spark.pid
+$SPARK_HOME/run_$1.sh
 
-$ONLINE_HOME/run_networked.sh $4 $5
+#echo $! > spark.pid
 
-PER_COUNTER_HOME/pcm.x -csv=$1_$2_$3_$4.csv -i=300000 &
+#$ONLINE_HOME/run_networked.sh $4 $5
 
-echo $! > pcm.pid
+#PER_COUNTER_HOME/pcm.x -csv=$1_$2_$3_$4.csv -i=300000 &
 
-wait ${cat spark.pid}
+#echo $! > pcm.pid
 
-$ONLINE_HOME/kill_networked.sh
+#wait ${cat spark.pid}
 
-sudo kill -9 ${cat pcm.pid}
+#$ONLINE_HOME/kill_networked.sh
+
+#sudo kill -9 ${cat pcm.pid}
 
 
 
