ca09038797a3101eaeddc978af2388591ca2dd47
Yuhao Li <yl408@clipper03.egr.duke.edu>
2017-07-04 12:25:09 -0700

    hahahaha

--- spark_scripts/run_kmeans.sh
+++ spark_scripts/run_kmeans.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 /home/yl408/spark/bin/spark-submit --num-executors 20 --jars /home/yl408/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
 --class org.apache.spark.examples.mllib.DenseKMeans --master spark://clipper04.egr.duke.edu:7077 \
 --deploy-mode client \
-/home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \  
--k 1000 ~/yuhao_datasets/USCensus1990.data.txt &
+/home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \
+-k 1000 ~/yuhao_datasets/USCensus1990.data.txt 
--- spark_scripts/run_pagerank.sh
+++ spark_scripts/run_pagerank.sh
@@ -1,5 +1,6 @@
+#! /bin/bash
 /home/yl408/spark/bin/spark-submit --num-executors 20 --jars /home/yl408/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
 --class org.apache.spark.examples.graphx.LiveJournalPageRank --master spark://clipper04.egr.duke.edu:7077 \
 --deploy-mode client \
-/home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \  
-/home/yl408/yuhao_datasets/40 --numEPart=72 &
+/home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \
+/home/yl408/yuhao_datasets/40 --numEPart=72 
