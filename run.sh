214afa7359cb0b604c3826566ea7237f0bc9f40a
Yuhao Li <yl408@clipper04.egr.duke.edu>
2017-07-06 07:10:40 -0700

    update scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-
+## parameter: 1. worker URL 2. spark application 3. online application 4. online core 5.QPS 6.MAX-REQ
 SPARK_HOME=$HOME/pb_scripts/spark_scripts
 ONLINE_HOME=$HOME/tailbench-v0.9/$3
 # setup 
 PER_COUNTER_HOME=$HOME/scripts/bash_scripts/IntelPerformanceCounterMonitorV2.8/
 
-ssh $1 $SPARK_HOME/run_$2.sh $3 &
+ssh $1 $SPARK_HOME/run_$2.sh &
 
 echo $! > spark.pid
 
--- spark_scripts/run_svm.sh
+++ spark_scripts/run_svm.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 /home/yl408/spark/bin/spark-submit --num-executors 20 --jars /home/yl408/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
---class org.apache.spark.examples.mllib.LinearRegression --master spark://clipper04.egr.duke.edu:7077 \
+--class org.apache.spark.examples.mllib.LinearRegression --master spark://clipper03.egr.duke.edu:7077 \
 --deploy-mode client \
 /home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \
 ~/yuhao_datasets/kdda --algorithm SVM --regType L2 --regParam 1.0 &
@@ -9,4 +9,4 @@ echo $! > 1.pid
 
 wait $(cat 1.pid)
 
-ssh clipper03 '/home/yl408/pb_scripts/kill.sh xapian'
+ssh clipper04 '/home/yl408/pb_scripts/kill.sh xapian'
