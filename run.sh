21dfbf291ef71eb371bae525599c17b5be798796
Dan Sun <dansun47@gmail.com>
2017-07-06 09:09:23 -0700

    modified run.sh and run_kmeans.sh to better suit working with dan

--- run.sh
+++ run.sh
@@ -1,26 +1,66 @@
 #! /bin/bash
 
-SPARK_HOME=$HOME/pb_scripts/spark_scripts
-ONLINE_HOME=$HOME/tailbench-v0.9/$3
-# setup 
-PER_COUNTER_HOME=$HOME/scripts/bash_scripts/IntelPerformanceCounterMonitorV2.8/
+#setup
+SPARK_HOME=$HOME/gitRepo/scripts/spark_scripts
+ONLINE_HOME=$HOME/gitRepo/tailbench/moses
+PER_COUNTER_HOME=$HOME/bash_scripts/IntelPerformanceCounterMonitorV2.8
 
-ssh $1 $SPARK_HOME/run_$2.sh $3 &
+#check
+if ! [ -d ${SPARK_HOME} ]
+then
+	echo "SPARK_HOME path ${SPARK_HOME} does not exist"
+	exit 1
+fi
 
-echo $! > spark.pid
+if ! [ -d ${ONLINE_HOME} ]
+then
+        echo "ONLINE_HOME path ${ONLINE_HOME} does not exist"
+        exit 1
+fi
 
-$ONLINE_HOME/run_networked.sh $4 $5 $6 &
+if ! [ -d ${PER_COUNTER_HOME} ]
+then
+        echo "PER_COUNTER_HOME path ${PER_COUNTER_HOME} does not exist"
+        exit 1
+fi
 
+if [ "$#" -ne 6 ]
+then
+	echo "To be used on server to deploy LC Applicatoin"
+	echo "Please call the with the following format:"
+	echo "./run.sh [QPS SERVERCORES CLIENTCORES SERVER SPARKAPP]"
+	exit 1
+fi
+#parameters
+QPS=$1
+SERVERCORES=$2
+CLIENTCORES=$3
+SERVER=$4
+SPARKAPP=$5
+
+WARMUPREQS=$(( 20 * ${QPS} ))
+#disable NMI watchdog for performance counter
+sudo bash -c "echo 0 > /proc/sys/kernel/nmi_watchdog"
+
+#start moses
+$ONLINE_HOME/run_networked.sh 2 0 ${WARMUPREQS} ${SERVERCORES} \
+				${QPS} 1 ${CLIENTCORES} &
+#start performance counter
 sudo $PER_COUNTER_HOME/pcm.x -csv=$2_$3_$5.csv -i=300000 &
-
 echo $! > pcm.pid
 
-#wait ${cat spark.pid}
+sleep 20 #wait for moses to stablize
+#start spark job
+ssh ${SERVER} $SPARK_HOME/run_${SPARKAPP}.sh $(hostname) &
 
-#$ONLINE_HOME/kill_networked.sh
 
-#sudo kill -9 ${cat pcm.pid}
 
+wait $(cat $ONLINE_HOME/server.pid) #wait for server to be killed by spark script
+
+sudo kill -9 ${cat pcm.pid}
+
+#enable NMI watchdog
+sudo bash -c "echo 1 > /proc/sys/kernel/nmi_watchdog" 
 
 
 
--- spark_scripts/run_kmeans.sh
+++ spark_scripts/run_kmeans.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
-/home/yl408/spark/bin/spark-submit --num-executors 20 --jars /home/yl408/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
---class org.apache.spark.examples.mllib.DenseKMeans --master spark://clipper04.egr.duke.edu:7077 \
+LCSERVER=$1
+ONLINE_HOME=$2
+
+/home/ds318/gitRepo/spark/bin/spark-submit --num-executors 20 --jars /home/ds318/gitRepo/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
+--class org.apache.spark.examples.mllib.DenseKMeans --master spark://$(hostname):7077 \
 --deploy-mode client \
-/home/yl408/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \
+/home/ds318/gitRepo/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.1.0.jar \
 -k 1000 ~/yuhao_datasets/USCensus1990.data.txt &
 
 echo $! > 1.pid
 
 wait $(cat 1.pid)
 
-ssh clipper03 '/home/yl408/pb_scripts/kill.sh xapian'
+ssh ${LCSERVER} "/home/ds318/gitRepo/tailbench/moses/kill_server.sh"
 
