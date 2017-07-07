e424c427a32d38a4289155ca682af480dbc020be
Dan Sun <dansun47@gmail.com>
2017-07-07 11:08:15 -0700

    modified run.sh, added loop_run.sh to collect data automatically; run_kmeas.sh is also modified to produce output

---
+++ loop_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+#parameter parsing
+if [ "$#" -ne 2 ]
+then
+        echo "Please call the with the following format:"
+        echo "./loop_run.sh [SERVER SPARKAPP]"
+        exit 1
+fi
+#arguments
+SERVER=$1
+SPARKAPP=$2
+
+#parameters
+SERVERCORES=21-23
+CLIENTCORES=9-11
+SPARKCORES=12-20,36-44
+DATAFOLDER="core9"
+
+if [ -d ${DATAFOLDER} ]
+then
+	rm -r ${DATAFOLDER}
+fi
+mkdir ${DATAFOLDER}	
+touch ${DATAFOLDER}/setup.txt
+echo -e "SERVER=${SERVER}\nSPARKAPP=${SPARKAPP}\nSERVERCORES=${SERVERCORES}" | tee -a ${DATAFOLDER}/setup.txt
+echo -e "CLIENTCORES=${CLIENTCORES}\nSPARKCORES=${SPARKCORES}\nDATAfOLDER=${DATAFOLDER}" | tee -a ${DATAFOLDER}/setup.txt
+
+for QPS in {100..1000..100}
+do
+	source ./run.sh ${QPS} ${SERVERCORES} ${CLIENTCORES} ${SERVER} ${SPARKAPP}
+	mv ${SPARKAPP}_${QPS}.csv ${DATAFOLDER}/.
+	mv lats.bin ${DATAFOLDER}/${QPS}.bin
+	sleep 10
+done
+
--- spark_scripts/run_kmeans.sh
+++ spark_scripts/run_kmeans.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
-LCSERVER=$1
-ONLINE_HOME=$2
+if [ -z "$1" ]
+then
+	exit 1
+fi
 
+LCSERVER=$1
+
+
+echo "-----------starting kmeans---------------"
+echo "LCSERVER=${LCSERVER}
+
+"
 /home/ds318/gitRepo/spark/bin/spark-submit --num-executors 20 --jars /home/ds318/gitRepo/spark/examples/target/scala-2.11/jars/scopt_2.11-3.3.0.jar \
 --class org.apache.spark.examples.mllib.DenseKMeans --master spark://$(hostname):7077 \
 --deploy-mode client \
@@ -9,8 +18,9 @@ ONLINE_HOME=$2
 -k 1000 ~/yuhao_datasets/USCensus1990.data.txt &
 
 echo $! > 1.pid
-
+echo "waiting for $(cat 1.pid)"
 wait $(cat 1.pid)
-
+rm 1.pid
+echo "------------kmeans finished---------"
 ssh ${LCSERVER} "/home/ds318/gitRepo/tailbench/moses/kill_server.sh"
 
