7800b77533695ce9a54ab780702e04a184bb26cb
Dan Sun <dansun47@gmail.com>
2017-07-11 14:54:01 -0400

    working on plotting

--- parse_csv.py
+++ parse_csv.py
@@ -10,58 +10,49 @@ import csv
 #import plotly.graph_objs as go 
 import numpy as np
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
 from scipy.interpolate import griddata
 
-import os.path
+number_core = 48
 
-#helper functions
-def getColNum (letters): #take csv column index as input, output corresponding number to use
-	sum = 0
-	for letter in letters:
-		sum *= 10
-		numerical = ord(letter) - 64
-		if numerical < 1  or numerical > 26:
-			return -1
-		else:
-			sum += numerical
-	return sum - 1
-
-
-#parameters
 if (len(sys.argv) < 2):
 	print "should provide input file path"
 	exit(1)
 
+
+
 file_path = str(sys.argv[1])
-if file_path.endswith('/'):
-	file_path = file_path[:-1]
-if !os.path.isdir(file_path):
-	print "Please specify a valid file path"
-	exit(1)
 
-COL_PER_CORE=16
-
-LS_CORES = [21,22,23]
-BE_CORES = 12,36#range(14,24)+range(38,48)
-number_core = 48
-## statrdisplacement
-L3Miss_displacment = 4
-L3Hit_displacement = 6
 
 load_level = range(100,1100,100)
+
+filenames = [file_path+"kmeans_xapian_12,13_"+str(load) for load in load_level]
+
+
+
 
 datapoints = list()
 
 load_llc = dict()
 
+fig,ax = plt.subplots()
+
 for load in load_level:
-	filename = file_path+"/kmeans_"+str(load)+".csv"
+
+
+	#print "--------load----------------------"+ str(load)
+	filename = file_path+"kmeans_xapian_"+str(load)+".csv"
+
+	#csv_file = csv.reader(filename,delimiter=' ',quotechar='|')
+	csv_file = open(filename,'r') 
 	core_start_column = [0] * number_core
-	core_start_column[0] = getColNum("CV")
-	for i in range(1,number_core):
-		core_start_column[i] = core_start_column[i-1] + COL_PER_CORE
+	core_start_column[0] = 26*3 + 6 - 1
+
 
 	be_ll3_cache_access_number = list()
 	be_ll3_cache_miss_number = list()
@@ -70,89 +61,162 @@ for load in load_level:
 	ls_ll3_cache_access_number = list()
 	ls_ll3_cache_miss_number = list()
 	ls_ll3_cache_miss_rate = list()
-	with open(filename, 'rb') as csvfile:
-		dataReader = csv.reader(csvfile, delimiter=';')
+
+	for i in range(1,48):
+		core_start_column[i] = core_start_column[i-1] + 16
+
+
+	## statrdisplacement
+	L3Miss_displacment = 4
+	L3Hit_displacement = 6
+
+	ls_cores = [12,13]
+	be_cores = range(14,24)+range(38,48)
+
 	i = 0
-	for row in csvfile:
+	for Row in csv_file:
 		#print Row
-		i = i + 1
-		if i <= 2: #ignore the first two rows
+		row = Row.split(';')
+
+		#print i
+		if i < 2:
+			i = i + 1
 			continue
 
+		i = i + 1
 		L3Miss_sum = 0
 		L3Hit_sum = 0
-
-
-		for ls_core in LS_CORES: #counting for server thread pool?
+		for ls_core in ls_cores:
 			start_column = core_start_column[ls_core]
-			L3Miss_column = start_column+L3Miss_displacment
-			L3Hit_column = start_column+L3Hit_displacement
-			#print row[L3Miss_column]
-			L3Miss = int(row[L3Miss_column])
-			L3Hit = int(row[L3Hit_column])
-			#L3access = L3Miss / (1-L3Hit) #clarify?
-			L3Miss_sum += L3Miss
-			L3Hit_sum += L3Hit
+			L3Miss_location = start_column+L3Miss_displacment
+			L3Hit_location = start_column+L3Hit_displacement
+			#print row[L3Miss_location]
+			L3Miss = float(row[L3Miss_location])
+			L3Hit = float(row[L3Hit_location])
+			
+			##exceptional case ignore it
+			if(L3Hit == 1):
+				L3Miss_modified = 0
+				L3access = 0
+			else:
+				L3Miss_modified = L3Miss
+				L3access = L3Miss / (1-L3Hit)
 
-		L3Access = L3Miss_sum + L3Hit_sum
-		ls_L3Miss_rate = L3Miss_sum / L3Access
+			L3Miss_sum += L3Miss_modified
+			L3Hit_sum += L3access*L3Hit
+
+		if (L3access == 0):
+			ls_L3Miss_rate = 1
+		else:
+			ls_L3Miss_rate = L3Miss_sum / (L3Miss_sum + L3Hit_sum)
 		ls_ll3_cache_miss_number.append(L3Miss_sum)
-		ls_ll3_cache_access_number.append(L3Access)
+		ls_ll3_cache_access_number.append(L3Hit_sum)
 		ls_ll3_cache_miss_rate.append(ls_L3Miss_rate)
 
 		L3Miss_sum = 0
 		L3Hit_sum = 0
 
-		for be_core in BE_CORES:
+		for be_core in be_cores:
 			start_column = core_start_column[be_core]
-			L3Miss_colmun = start_column+L3Miss_displacment
-			L3Hit_column = start_column+L3Hit_displacement
-			L3Miss = int(row[L3Miss_location])
-			L3Hit = int(row[L3Hit_location])
-			#L3access = L3Miss / (1-L3Hit) #clarify?
-			L3Miss_sum += L3Miss
-			L3Hit_sum += L3Hit
+			L3Miss_location = start_column+L3Miss_displacment
+			L3Hit_location = start_column+L3Hit_displacement
+			L3Miss = float(row[L3Miss_location])
+			L3Hit = float(row[L3Hit_location])
 
-		L3Access = L3Miss_sum + L3Hit_sum
-		be_L3Miss_rate = L3Miss_sum / L3Access
+			if(L3Hit == 1):
+				L3Miss_modified = 0
+				L3access = 0
+			else:
+				L3Miss_modified = L3Miss
+				L3access = L3Miss / (1-L3Hit)
+
+			L3Miss_sum += L3Miss_modified
+			L3Hit_sum += L3access*L3Hit
+
+
+		if (L3access == 0):
+			be_L3Miss_rate = 1
+		else:
+			be_L3Miss_rate = L3Miss_sum / (L3Miss_sum + L3Hit_sum)
+			
 		be_ll3_cache_miss_number.append(L3Miss_sum)
-		be_ll3_cache_access_number.append(L3Access)
+		be_ll3_cache_access_number.append(L3Hit_sum)
 		be_ll3_cache_miss_rate.append(be_L3Miss_rate)
-		
-		datapoints.append((load,ls_ll3_cache_miss_rate,be_ll3_cache_miss_rate))
+		datapoints.append((load,L3Miss_sum,ls_L3Miss_rate))
 	
-	length = ls_ll3_cache_miss_rate 
+	length = len(ls_ll3_cache_miss_rate) 
 
-	t = np.linspace(0,length,length*2)
+	t = np.linspace(0,length,length)
+	#print len(ls_ll3_cache_miss_rate)
+	#print len(be_ll3_cache_miss_rate)
 
-	plt.plot(t,lls_ll3_cache_access_number,'r')
-	plt.plot(t,be_ll3_cache_access_number,'b')
-	plt.title(str(load))	
-	plt.savefig(str(load)+'.jpg')
+	if(load == 1000):
+		plt.plot(t,be_ll3_cache_miss_rate,'b',label='rate')
+		plt.plot(t,[a/max(be_ll3_cache_miss_number) for a in be_ll3_cache_miss_number],'r',label='number')
+	
 
+	#plt.plot(t,be_ll3_cache_miss_rate,'b')
+	#plt.title(str(load))	
+	#plt.savefig(filename+str(load)+'.jpg')
 
-#xlist = list()
-#value = list()
-
-#for point in datapoints:
-	#xlist.append(point[0],point[1])
-	#value.append(point[2])
-
-#xarr = np.array(xlist,dtype=dt)
-#varr = np.value(value,dtype=dt)
-
+	#plt.plot(ls_ll3_cache_miss_rate,be_ll3_cache_miss_number,'o')
+	#plt.title(str(load))	 
+	
 
 
-#grid_x,grd_y = np.mgrid(np.arange(100,1001,1),np.arange(0,1,0.01))
-
-#grid_z0 = griddata(xarr, varr, (grid_x, grid_y), method='nearest')
-
-#grid_z1 = griddata(xarr, varr, (grid_x, grid_y), method='linear')
-
-#grid_z2 = griddata(xarr, varr, (grid_x, grid_y), method='cubic')
+legend = ax.legend(loc='upper center', shadow=True)
+plt.title("batch last level cache miss rate at different load level")
+plt.savefig(file_path+"batch_miss.jpg")
+plt.clf()
 
 
-#plot_surface(X,Y,Z)
+xlist = list()
+value = list()
+
+max_be = 0
+for point in datapoints:
+	xlist.append([point[0],point[1]])
+	max_be = max(point[1],max_be)
+	value.append(point[2])
+
+
+dt = np.dtype('int,float')
+#print xlist
+xarr = np.array(xlist)
+
+varr = np.array(value)
+
+
+
+
+
+grid_x,grid_y = np.meshgrid(np.arange(100,1001,1),np.arange(0,max_be,0.5))
+
+#print grid_x.shape
+#print grid_y.shape
+#print xarr.shape
+#print varr.shape
+
+
+grid_z0 = griddata(xarr, varr, (grid_x, grid_y), method='nearest')
+
+grid_z1 = griddata(xarr, varr, (grid_x, grid_y), method='linear')
+
+grid_z2 = griddata(xarr, varr, (grid_x, grid_y), method='cubic')
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+surf = ax.plot_surface(grid_x,grid_y,grid_z0)
+
+
+#ax.set_zlim(-1.01, 1.01)
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+plt.savefig(file_path+'ls_be_load.jpg')
+
 #plot figure
 
 
--- parse_csv_single.py
+++ parse_csv_single.py
@@ -5,16 +5,37 @@ import matplotlib.pyplot as plt
 import sys
 import csv 
 
-
 #import plotly.plotly as py
 #import plotly.graph_objs as go 
-import numpy as np
-
-
 
 from scipy.interpolate import griddata
 
 import os.path
+
+
+
+params = {}
+NUMCORE=48
+CDISP = {   #dictionary for stats displacement for core
+	'EXEC' : 0,
+	'IPC' : 1,
+	'FREQ' : 2,
+	'AFREQ' : 3,
+	'L3MISS' : 4,
+	'L2MISS' : 5,
+	'L3HIT' : 6,
+	'L2HIT' : 7,
+	'L3CLK' : 8,
+	'L2CLK' : 9,
+	'C0res' : 10,
+	'C1res' : 11,
+	'C3res' : 12,
+	'C6res' : 13,
+	'C7res' : 14,
+	'TEMP' : 15
+}
+COL_PER_CORE=16
+
 
 #helper functions
 def getColNum (letters): #take csv column index as input, output corresponding number to use
@@ -28,108 +49,138 @@ def getColNum (letters): #take csv column index as input, output corresponding n
 			sum += numerical
 	return sum - 1
 
+def readSetup (filePath):
+	with open(filePath, 'r') as setup:
+		for line in setup:
+			line = line.strip()
+			param = line.split("=")
+			params[param[0]] = param[1]
 
-#parameters
-if (len(sys.argv) < 2):
-	print "should provide input file path"
-	exit(1)
-
-file_path = str(sys.argv[1])
-if file_path.endswith('/'):
-	file_path = file_path[:-1]
-if not os.path.isdir(file_path):
-	print "Please specify a valid file path"
-	exit(1)
-
-COL_PER_CORE=16
-
-LS_CORES = [21,22,23]
-BE_CORES = 12,36#range(14,24)+range(38,48)
-number_core = 48
-## statrdisplacement
-L3Miss_displacment = 4
-L3Hit_displacement = 6
-
-load_level = range(100,200,100)
-
-datapoints = list()
-
-load_llc = dict()
-
-for load in load_level:
-	filename = file_path+"/kmeans_"+str(load)+".csv"
-	core_start_column = [0] * number_core
-	core_start_column[0] = getColNum("CV")
-	for i in range(1,number_core):
-		core_start_column[i] = core_start_column[i-1] + COL_PER_CORE
-
-	be_ll3_cache_access_number = list()
-	be_ll3_cache_miss_number = list()
-	be_ll3_cache_miss_rate = list()
-
-	ls_ll3_cache_access_number = list()
-	ls_ll3_cache_miss_number = list()
-	ls_ll3_cache_miss_rate = list()
-	with open(filename, 'rb') as csvfile:
-		dataReader = csv.reader(csvfile, delimiter=';')
-		i = 0
-		for row in dataReader:
-			#print Row
-			i = i + 1
-			if i <= 2: #ignore the first two rows
-				continue
-
-			L3Miss_sum = 0
-			L3Hit_sum = 0
-
-
-			for ls_core in LS_CORES: #counting for server thread pool?
-				start_column = core_start_column[ls_core]
-				L3Miss_column = start_column+L3Miss_displacment
-				L3Hit_column = start_column+L3Hit_displacement
-				#print row[L3Miss_column]
-				L3Miss = float(row[L3Miss_column])
-				L3Hit = float(row[L3Hit_column])
-				#L3access = L3Miss / (1-L3Hit) #clarify?
-				L3Miss_sum += L3Miss
-				L3Hit_sum += L3Hit
-
-			L3Access = L3Miss_sum + L3Hit_sum
-			ls_L3Miss_rate = L3Miss_sum / L3Access
-			ls_ll3_cache_miss_number.append(L3Miss_sum)
-			ls_ll3_cache_access_number.append(L3Access)
-			ls_ll3_cache_miss_rate.append(ls_L3Miss_rate)
-
-			L3Miss_sum = 0
-			L3Hit_sum = 0
-
-			for be_core in BE_CORES:
-				start_column = core_start_column[be_core]
-				L3Miss_colmun = start_column+L3Miss_displacment
-				L3Hit_column = start_column+L3Hit_displacement
-				L3Miss = float(row[L3Miss_colmun])
-				L3Hit = float(row[L3Hit_column])
-				#L3access = L3Miss / (1-L3Hit) #clarify?
-				L3Miss_sum += L3Miss
-				L3Hit_sum += L3Hit
-
-			L3Access = L3Miss_sum + L3Hit_sum
-			be_L3Miss_rate = L3Miss_sum / L3Access
-			be_ll3_cache_miss_number.append(L3Miss_sum)
-			be_ll3_cache_access_number.append(L3Access)
-			be_ll3_cache_miss_rate.append(be_L3Miss_rate)
+def getCores (corestr):
+	cores = list()
+	corestr = corestr.strip()
+	groups = corestr.split(",")
+	for group in groups:
+		bounds = group.split("-")
+		if len(bounds) == 1:
+			cores.append(int(bounds[0]))
+		elif len(bounds) == 2:
+			for c in range(int(bounds[0]),int(bounds[1])+1):
+				cores.append(c)
+		else:
+			print ('getCores: parsing error')
+			exit(1)
+	return cores
 			
-			datapoints.append((load,ls_ll3_cache_miss_rate,be_ll3_cache_miss_rate))
+
+if (len(sys.argv) < 2):
+	print "should provide input directory path"
+	exit(1)
+
+dirPath = str(sys.argv[1])
+if dirPath.endswith('/'):
+	dirPath = dirPath[:-1]
+if not os.path.isdir(dirPath):
+	print "Please specify a valid directory path"
+	exit(1)
+
+readSetup(dirPath + "/setup.txt")
+
+ls_cores = getCores(params["SERVERCORES"])
+be_cores = getCores(params["SPARKCORES"])
+
+print ls_cores
+print be_cores
+
+
+
+# LS_CORES = [21,22,23]
+# BE_CORES = 12,36#range(14,24)+range(38,48)
+
+# ## stat displacement
+# L3Miss_displacment = 4
+# L3Hit_displacement = 6
+
+# load_level = range(100,200,100)
+
+# datapoints = list()
+
+# load_llc = dict()
+
+# for load in load_level:
+# 	filename = file_path+"/kmeans_"+str(load)+".csv"
+# 	core_start_column = [0] * number_core
+# 	core_start_column[0] = getColNum("CV")
+# 	for i in range(1,number_core):
+# 		core_start_column[i] = core_start_column[i-1] + COL_PER_CORE
+
+# 	be_ll3_cache_access_number = list()
+# 	be_ll3_cache_miss_number = list()
+# 	be_ll3_cache_miss_rate = list()
+
+# 	ls_ll3_cache_access_number = list()
+# 	ls_ll3_cache_miss_number = list()
+# 	ls_ll3_cache_miss_rate = list()
+# 	with open(filename, 'rb') as csvfile:
+# 		dataReader = csv.reader(csvfile, delimiter=';')
+# 		i = 0
+# 		for row in dataReader:
+# 			#print Row
+# 			i = i + 1
+# 			if i <= 2: #ignore the first two rows
+# 				continue
+
+# 			L3Miss_sum = 0
+# 			L3Hit_sum = 0
+
+
+# 			for ls_core in LS_CORES: #counting for server thread pool?
+# 				start_column = core_start_column[ls_core]
+# 				L3Miss_column = start_column+L3Miss_displacment
+# 				L3Hit_column = start_column+L3Hit_displacement
+# 				#print row[L3Miss_column]
+# 				L3Miss = float(row[L3Miss_column])
+# 				L3Hit = float(row[L3Hit_column])
+# 				#L3access = L3Miss / (1-L3Hit) #clarify?
+# 				L3Miss_sum += L3Miss
+# 				L3Hit_sum += L3Hit
+
+# 			L3Access = L3Miss_sum + L3Hit_sum
+# 			ls_L3Miss_rate = L3Miss_sum / L3Access
+# 			ls_ll3_cache_miss_number.append(L3Miss_sum)
+# 			ls_ll3_cache_access_number.append(L3Access)
+# 			ls_ll3_cache_miss_rate.append(ls_L3Miss_rate)
+
+# 			L3Miss_sum = 0
+# 			L3Hit_sum = 0
+
+# 			for be_core in BE_CORES:
+# 				start_column = core_start_column[be_core]
+# 				L3Miss_colmun = start_column+L3Miss_displacment
+# 				L3Hit_column = start_column+L3Hit_displacement
+# 				L3Miss = float(row[L3Miss_colmun])
+# 				L3Hit = float(row[L3Hit_column])
+# 				#L3access = L3Miss / (1-L3Hit) #clarify?
+# 				L3Miss_sum += L3Miss
+# 				L3Hit_sum += L3Hit
+
+# 			L3Access = L3Miss_sum + L3Hit_sum
+# 			be_L3Miss_rate = L3Miss_sum / L3Access
+# 			be_ll3_cache_miss_number.append(L3Miss_sum)
+# 			be_ll3_cache_access_number.append(L3Access)
+# 			be_ll3_cache_miss_rate.append(be_L3Miss_rate)
+			
+# 			datapoints.append((load,ls_ll3_cache_miss_rate,be_ll3_cache_miss_rate))
 		
-	length = len(ls_ll3_cache_miss_rate) 
+# 	length = len(ls_ll3_cache_miss_rate) 
 
-	t = range(0, length)
+# 	t = range(0, length)
 
-	plt.plot(t,ls_ll3_cache_access_number,'r')
-	plt.plot(t,be_ll3_cache_access_number,'b')
-	plt.title(str(load))	
-	plt.show()
-	#plt.savefig(str(load)+'.jpg')
+# 	plt.plot(t,ls_ll3_cache_access_number,'r')
+# 	plt.plot(t,be_ll3_cache_access_number,'b')
+# 	plt.title(str(load))	
+# 	plt.show()
+# 	#plt.savefig(str(load)+'.jpg')
 
 
 
