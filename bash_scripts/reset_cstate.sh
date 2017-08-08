3dc57dd28b7232ad6923556ed49ded0a2e0c37f4
Dan Sun <dansun47@gmail.com>
2017-08-07 19:58:37 -0700

    data from clipper

--- bash_scripts/reset_cstate.sh
+++ bash_scripts/reset_cstate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-kill $(cat ${DIR}/setcstate.pid)
-rm ${DIR}/setcstate.pid
+kill $(cat ${DIR}/setcstate_$( hostname ).pid)
+rm ${DIR}/setcstate_$( hostname ).pid
--- bash_scripts/set_cstate.sh
+++ bash_scripts/set_cstate.sh
@@ -9,4 +9,4 @@ CORE=$1
 LATENCY=$2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 taskset -c ${CORE} ${DIR}/set_cstate ${LATENCY} &
-echo $! > ${DIR}/setcstate.pid
+echo $! > ${DIR}/setcstate_$( hostname ).pid
