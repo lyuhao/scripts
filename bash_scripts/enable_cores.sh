8e981920957c02a8c594040cc183b9cf550a1577
Dan Sun <dansun47@gmail.com>
2017-08-23 23:22:19 -0400

    local adjustment

--- bash_scripts/enable_cores.sh
+++ bash_scripts/enable_cores.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in `seq 1 47`;
+for i in `seq 1 15`;
 do
 echo 1 > /sys/devices/system/cpu/cpu$i/online
 done
--- expFinish.sh
+++ expFinish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #turn on all cores
-sudo ~/gitRepo/scripts/bash_scripts/enable_cores.sh
+sudo ~/scripts/bash_scripts/enable_cores.sh
 
 #turn on turbo boost
-sudo ~/gitRepo/scripts/bash_scripts/turbo-boost.sh enable
+sudo ~/scripts/bash_scripts/turbo-boost.sh enable
 
 #disable c-states
-sudo ~/gitRepo/scripts/bash_scripts/reset_cstate.sh 
+sudo ~/scripts/bash_scripts/reset_cstate.sh 
 
 #turn on puppet agent
 #sudo puppet agent --enable
