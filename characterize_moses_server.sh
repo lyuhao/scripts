dc5a9acdbf109bf7f2376fa8219972ec97f74a73
Dan Sun <dansun47@gmail.com>
2017-09-13 16:18:09 -0400

    added script for charaterizing server on bcl machines

--- characterize_moses_server.sh
+++ characterize_moses_server.sh
@@ -12,43 +12,44 @@ then
         exit 1
 fi
 
+if ! [ -d ${SCRIPT_HOME} ]
+then
+        echo "SCRIPT_HOME path ${SCRIPT_HOME} does not exist"
+        exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SERVER_THREADS=$1
+SERVER_CORES=$2
+SERVER_MACHINE=$(hostname)
+LAUNCH_SERVER_SCRIPT_CORE=4
 
-# DATADIR=${DIR}/data_base/Threads2
-
-
-CORES_SERVER=$2
-
-NTHREAD_CLIENT=1
-CORES_CLIENT=9-11
-
-mkdir ${DATADIR}
-
-sudo cpupower -c "${CORES_SERVER},${CORES_CLIENT}" frequency-set -g performance
+CLIENT_MACHINE=bcl15-cmp-01.egr.duke.edu
+LAUNCH_CLIENT_SCRIPT_CORE=4
+CLIENT_CORES=5-7
+CLIENT_THREADS=1
 
 for QPS in {100..1000..50}
 do
-	MAXREQS=$((30 * ${QPS}))
-	WARMUPREQS=$((20 * ${QPS}))
-	source ${DIR}/run_networked.sh ${NTHREAD_SERVER} ${MAXREQS} \
-			${WARMUPREQS} ${CORES_SERVER} ${QPS} \
-			${NTHREAD_CLIENT} ${CORES_CLIENT}
-	sudo cp lats.bin ${DATADIR}/QPS${QPS}.bin
-	echo -e "\n\n"
-	sleep 5 #wait between execution
-done
+	MAXREQS=$((400 * ${QPS}))
+	WARMUPREQS=$((50 * ${QPS}))
+	#launch server
+	cd ${ONLINE_HOME}
+	sudo taskset -c ${LAUNCH_SERVER_SCRIPT_CORE} \
+		./launch_server.sh ${SERVER_THREADS} ${MAXREQS} ${WARMUPREQS} ${SERVER_CORES} &
+	echo $! > server.pid
+	sleep 5s #wait for server to start up
 
-#restore cpu gorvernor
-sudo cpupower -c "${CORES_SERVER},${CORES_CLIENT}" frequency-set -g ondemand
+	#launch client
+	ssh ds318@${CLIENT_MACHINE} "${sudo taskset -c ${LAUNCH_CLIENT_SCRIPT_CORE} ${ONLINE_HOME}/launch_client.sh ${QPS} ${CLIENT_THREADS} ${CLIENT_CORES} ${SERVER_MACHINE}" &
 
-ANALYSISFILE=${DATADIR}/analysis.txt
-touch ${ANALYSISFILE}
+	# wait for server to finish
+	wait $(cat server.pid)
+	rm server.pid
+	sleep 10s #wait for client to dump stats
 
-for QPS in {100..1000..50}
-do
-	echo -e QPS: ${QPS} | tee -a ${ANALYSISFILE}
-	./parselatsbin.py ${DATADIR}/QPS${QPS}.bin | tee -a ${ANALYSISFILE}
-	echo -e '' | tee -a ${ANALYSISFILE}
+	DATADIR=${SCRIPT_HOME}/server_characterization_data/Thread${SERVER_THREADS}/QPS${QPS}
+	ssh ds318@${CLIENT_MACHINE} "mkdir ${DATADIR}"
+	ssh ds318@${CLIENT_MACHINE} "cp lats.bin DATADIR/q${QPS}.bin"
 done
