862ca49153c16e1b037a8c2b44b97dd9ffa45c10
Dan Sun <dansun47@gmail.com>
2017-09-20 16:40:21 -0400

    changed input for correlation to kdda

--- spark_scripts/local_mode/run_correlation.sh
+++ spark_scripts/local_mode/run_correlation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo ~/bash_scripts/IntelPerformanceCounterMonitorV2.8/pcm.x -r -i=1
 #sudo ~/bash_scripts/IntelPerformanceCounterMonitorV2.8/pcm.x -csv=/scratch/sf117/correlation_sprint.csv -i=30000 &
-taskset -c $1 ~/spark/bin/run-example org.apache.spark.examples.mllib.Correlations --input ~/yuhao_datasets/kdda_part &
+taskset -c $1 ~/spark/bin/run-example org.apache.spark.examples.mllib.Correlations --input ~/yuhao_datasets/kdda &
 echo $! > correlation.pid
 
