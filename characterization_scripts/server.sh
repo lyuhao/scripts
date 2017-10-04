b997e3c21624fd2239a25a01b589bb77a7e8d35c
Dan Sun <dansun47@gmail.com>
2017-10-04 16:23:19 -0400

    modified server.sh so that it launches moses server directly instead of calling launch_server.sh inside moses directory

--- characterization_scripts/server.sh
+++ characterization_scripts/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This scripts is used to launch the server of tailbench job
+#This scripts is used to launch the server of moses
 
 source /home/ds318/scripts/bash_helpers/readPathsConfiguration.sh
 
@@ -21,48 +21,22 @@ SERVER_THREADS=$1
 MAXREQS=$2
 WARMUPREQS=$3
 
-#launch server
-cd ${ONLINE_HOME}
-source ./launch_server.sh ${SERVER_THREADS} ${MAXREQS} ${WARMUPREQS} ${SERVER_CORES}
+source ${ONLINE_HOME}/../configs.sh
 
-
-#!/bin/bash
-
-SERVER_THREADS=$1
-TBENCH_MAXREQS=$2
-TBENCH_WARMUPREQS=$3
-SERVER_CORES=$4
-
-if [ $# -ne 4 ]; then
-        echo -e "Please call the program with the in the following format:"
-        echo -e "${BASH_SOURCE[0]} [SERVERTHREADS MAXREQS WARMUPREQS SERVERCORES]"
-        exit 1
-fi
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "DIR=${DIR}"
-source ${DIR}/../configs.sh
-
-BINDIR=${DIR}/bin
+BINDIR=${ONLINE_HOME}/bin
 
 # Setup
-cp ${DIR}/moses.ini.template ${DIR}/moses.ini
-sed -i -e "s#@DATA_ROOT#$DATA_ROOT#g" ${DIR}/moses.ini
+cp ${ONLINE_HOME}/moses.ini.template ${ONLINE_HOME}/moses.ini
+sed -i -e "s#@DATA_ROOT#$DATA_ROOT#g" ${ONLINE_HOME}/moses.ini
 
 # Launch Server
 TBENCH_MAXREQS=${TBENCH_MAXREQS} TBENCH_WARMUPREQS=${TBENCH_WARMUPREQS} TBENCH_NCLIENTS=1 \
 TBENCH_SERVER=$(hostname) \
-taskset -c ${SERVER_CORES} ${BINDIR}/moses_server_networked \
-    -config ${DIR}/moses.ini \
+${BINDIR}/moses_server_networked \
+    -config ${ONLINE_HOME}/moses.ini \
     -input-file ${DATA_ROOT}/moses/testTerms \
     -threads ${SERVER_THREADS} -num-tasks 7500000 -verbose 0 &
 
-echo $! > ${DIR}/server.pid
+echo $! > moses_server.pid
 
-sudo chrt -r -p 99 $(cat ${DIR}/server.pid)
-
-wait $(cat ${DIR}/server.pid)
-
-# Cleanup
-rm ${DIR}/server.pid
+sudo chrt -r -p 99 $(cat moses_server.pid)
